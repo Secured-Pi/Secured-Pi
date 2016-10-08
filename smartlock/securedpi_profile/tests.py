bb2ea4fcf38232ab09e899ae74613bb2494c0a6c
tanyaweaver <tanya83@uw.edu>
2016-10-08 14:58:29 -0700

    added tests for SecuredpiProfile model

--- smartlock/securedpi_profile/tests.py
+++ smartlock/securedpi_profile/tests.py
@@ -1,1 +1,38 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from securedpi_profile.models import SecuredpiProfile
+from django.urls import reverse
+import os
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+
+
+class SecurepiProfileTestCase(TestCase):
+    """Create test class for SecuredpiProfile model."""
+    def setUp(self):
+        """Set up a fake user and profile."""
+        self.user = User(username='test')
+        self.user.set_password('test')
+        self.user.save()
+
+    def test_user_exists(self):
+        """Prove the user exists."""
+        self.assertTrue(self.user is not None)
+
+    def test_username(self):
+        """Prove the username is correct."""
+        self.assertEqual(self.user.username, 'test')
+
+    def test_profile_exists(self):
+        """Prove that a profile is automatically created for a new user."""
+        self.assertTrue(self.user.profile is not None)
+
+    def test_profile_is_attached_to_right_user(self):
+        """Prove that the profileis attached to the right user."""
+        self.assertEqual(self.user.profile.user.username, 'test')
+
+    def test_profile_attributes(self):
+        """Prove that SecuredpiProfile model has expected attributes."""
+        attrs = ['first_name', 'last_name', 'address', 'email']
+        for attr in attrs:
+            self.assertTrue(hasattr(self.user.profile, attr))
