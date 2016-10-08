e754840256a95fc10dd59d4c1e2d3ba712e85229
tanyaweaver <tanya83@uw.edu>
2016-10-08 14:34:07 -0700

    created a postgres db, made initial migration.

--- smartlock/securedpi/settings.py
+++ smartlock/securedpi/settings.py
@@ -74,10 +74,15 @@ WSGI_APPLICATION = 'securedpi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'securedpi',
+        'USER': os.environ.get("USER"),
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
--- smartlock/securedpi_profile/admin.py
+++ smartlock/securedpi_profile/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from securedpi_profile.models import SecuredpiProfile
 
-# Register your models here.
+admin.site.register(SecuredpiProfile)
