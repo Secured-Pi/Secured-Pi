e61bea1e7363fdbb6c39d4b8fa409d0cebaa8020
tanyaweaver <tanya83@uw.edu>
2016-10-10 15:56:54 -0700

    added dj-database-url to the DATABASE in settings

--- securedpi/settings.py
+++ securedpi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -72,17 +73,22 @@ WSGI_APPLICATION = 'securedpi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'securedpi',
-        'USER': os.environ.get("USER"),
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        default='postgres://{}@localhost:5432/securedpi'
+        .format(os.environ.get('USER')))
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'securedpi',
+#         'USER': os.environ.get("USER"),
+#         'PASSWORD': '',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
