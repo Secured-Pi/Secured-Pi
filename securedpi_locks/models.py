2614374924716cd55a46469eb9667b8f564cb9ec
tanyaweaver <tanya83@uw.edu>
2016-10-10 14:43:41 -0700

    added date_created and date_modified fields to Lock model, migrated

--- securedpi_locks/models.py
+++ securedpi_locks/models.py
@@ -14,6 +14,8 @@ class Lock(models.Model):
     title = models.CharField(max_length=50)
     location = models.CharField(max_length=50)
     description = models.CharField(max_length=100, blank=True)
+    date_uploaded = models.DateField(auto_now_add=True)
+    data_modified = models.DateField(auto_now=True)
     raspberry_pi_id = models.CharField(max_length=50)
     web_cam_id = models.CharField(max_length=50, blank=True)
     is_locked = models.BooleanField(default=False)
