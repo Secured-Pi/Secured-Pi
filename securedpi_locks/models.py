6353292b4aeb6b230745e9502c74ec03cd022682
tanyaweaver <tanya83@uw.edu>
2016-10-10 22:20:15 -0700

    chnaged Lock model(added fields:facial_recognition, is_active, status=locked/unlocked/pending)

--- securedpi_locks/models.py
+++ securedpi_locks/models.py
@@ -11,14 +11,23 @@ class Lock(models.Model):
         related_name='locks',
         on_delete=models.CASCADE
     )
-    title = models.CharField(max_length=50)
-    location = models.CharField(max_length=50)
-    description = models.CharField(max_length=100, blank=True)
+    title = models.CharField(max_length=15)
+    location = models.CharField(max_length=25)
+    description = models.CharField(max_length=25, blank=True)
     date_created = models.DateField(auto_now_add=True)
     data_modified = models.DateField(auto_now=True)
-    raspberry_pi_id = models.CharField(max_length=50)
-    web_cam_id = models.CharField(max_length=50, blank=True)
-    is_locked = models.BooleanField(default=False)
+    raspberry_pi_id = models.CharField(max_length=20)
+    web_cam_id = models.CharField(max_length=20, blank=True)
+    status = models.CharField(
+        max_length=8,
+        choices=(
+            ('locked', 'locked'),
+            ('unlocked', 'unlocked'),
+            ('pending', 'pending'),
+        ),
+        default='unlocked')
+    is_active = models.BooleanField(default=False)
+    facial_recognition = models.BooleanField(default=False)
 
     def __str__(self):
         return 'Lock for {}'.format(self.user)
