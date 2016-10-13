a6f4475f9f0480017b45ad4328d7f17a3a26fe80
tanyaweaver <tanya83@uw.edu>
2016-10-12 17:55:17 -0700

    created edit lock view

--- securedpi_locks/urls.py
+++ securedpi_locks/urls.py
@@ -6,19 +6,9 @@ from securedpi_locks.models import Lock
 
 
 urlpatterns = [
-    # url(r'^(?P<pk>\d+)/$',
-    #     login_required(DetailView.as_view(
-    #         template_name='securedpi_locks/lock_details.html',
-    #         model=Lock,
-    #         context_object_name='lock'
-    #     )),
-    #     name='lock_details'),
-    # url(r'^manual/unlock/(?P<pk>\d+)/$',
-    #     login_required(views.manual_unlock),
-    #     name='manual_unlock'),
-    # url(r'^manual/lock/(?P<pk>\d+)/$',
-    #     login_required(views.manual_lock),
-    #     name='manual_lock'),
+    url(r'^(?P<pk>\d+)/edit/$',
+        login_required(views.EditLockView.as_view()),
+        name='edit_lock'),
     url(r'^manual-unlock/(?P<pk>\d+)/$',
         login_required(views.manual_action),
         kwargs={'action': 'unlock'},
--- securedpi_locks/views.py
+++ securedpi_locks/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import UpdateView
 from securedpi_locks.models import Lock
 from securedpi_events.models import Event
 import requests
 import json
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+
+
+class EditLockView(UpdateView):
+    """Define edit Lock class."""
+    template_name = 'securedpi_locks/edit_lock.html'
+    model = Lock
+    fields = ['name', 'location', 'description', 'is_active', 'facial_recognition']
+
+    # def get_form(self, form_class=None):
+    #     """
+    #     Modify 'photos' field in the form to show only user-specific photos.
+    #     """
+    #     form = super(EditAlbumView, self).get_form(form_class)
+    #     qs = form.fields['photos'].queryset
+    #     qs = qs.filter(user=self.request.user)
+    #     form.fields['photos'].queryset = qs
+    #     return form
+
+    def get_success_url(self):
+        """Set redirection after updating the album."""
+        url = reverse('dashboard')
+        return url
 
 
 def manual_action(request, **kwargs):
