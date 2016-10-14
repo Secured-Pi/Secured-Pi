650ad40274f08386b164089ff48eed4349bcbd55
tanyaweaver <tanya83@uw.edu>
2016-10-14 09:33:52 -0700

    username now shows up on the page

--- securedpi/settings.py
+++ securedpi/settings.py
@@ -173,7 +173,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
-LOGIN_REDIRECT_URL = '/dashboard/'
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 MEDIA_URL = '/media/'
--- securedpi/urls.py
+++ securedpi/urls.py
@@ -30,15 +30,15 @@ urlpatterns = [
     url(r'^events/', include('securedpi_events.urls')),
     url(r'^profile/', include('securedpi_profile.urls')),
     url(r'^facerec/', include('securedpi_facerec.urls')),
+    # url(r'^$',
+    #     TemplateView.as_view(template_name='securedpi/home_page.html'),
+    #     name='homepage'),
     url(r'^$',
-        TemplateView.as_view(template_name='securedpi/home_page.html'),
-        name='homepage'),
-    url(r'^dashboard/',
         login_required(DashboardView.as_view()),
         name='dashboard'),
-    url(r'^about/$',
-        TemplateView.as_view(template_name='securedpi/about_page.html'),
-        name='about'),
+    # url(r'^about/$',
+    #     TemplateView.as_view(template_name='securedpi/about_page.html'),
+    #     name='about'),
 ]
 
 urlpatterns += [
