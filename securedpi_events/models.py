11e4b5146052b3f97294d0847c1549a5446b9985
tanyaweaver <tanya83@uw.edu>
2016-10-14 12:13:50 -0700

    uncommented FR

--- securedpi_events/models.py
+++ securedpi_events/models.py
@@ -5,7 +5,7 @@ from securedpi_locks.models import Lock
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 import requests
-from securedpi_facerec.facial_recognition import facial_recognition
+#from securedpi_facerec.facial_recognition import facial_recognition
 from securedpi.settings import MEDIA_ROOT
 
 
@@ -30,41 +30,41 @@ class Event(models.Model):
         return 'Event for {}'.format(self.lock_id)
 
 
-@receiver(post_save, sender=Event)
-def start_FR(sender, **kwargs):
-    """
-    Initiate facial recognition method with received photo.
-
-    If the photo and RFID are recognized, make post request to rasp pi to
-    unlock.
-    """
-    event = kwargs['instance']
-    lock = Lock.objects.get(pk=event.lock_id)
-
-    if event.photo and lock.status == 'locked':
-        dj_decision = facial_recognition.test_individual(
-            event.photo.url,
-            verbose=True)
-        username = User.objects.get(pk=dj_decision[0]).username
-        print('**face recognized: ', dj_decision[0], ' as member ', username)
-        user_owns_lock = dj_decision[0] == lock.user.pk
-        confidence_acceptable = dj_decision[1] < 45
-        matching_rfid = event.RFID == lock.RFID
-        print('User has access to lock: ', user_owns_lock)
-        print('Confidence acceptable: ', confidence_acceptable, dj_decision[1])
-        print('RFID matches: ', matching_rfid)
-
-        if user_owns_lock and confidence_acceptable and matching_rfid:
-            serial = lock.serial
-            data = {
-                'event_id': event.pk,
-                'action': 'unlock',
-                'serial': serial,
-                'mtype': 'fr'
-            }
-            lock.status = 'pending'
-            lock.save()
-            print('User authorized, sending unlock request.')
-            requests.post('http://52.43.75.183:5000', json=data)
-            return
-        print('Access Denied.')
+# @receiver(post_save, sender=Event)
+# def start_FR(sender, **kwargs):
+#     """
+#     Initiate facial recognition method with received photo.
+#
+#     If the photo and RFID are recognized, make post request to rasp pi to
+#     unlock.
+#     """
+#     event = kwargs['instance']
+#     lock = Lock.objects.get(pk=event.lock_id)
+#
+#     if event.photo and lock.status == 'locked':
+#         dj_decision = facial_recognition.test_individual(
+#             event.photo.url,
+#             verbose=True)
+#         username = User.objects.get(pk=dj_decision[0]).username
+#         print('**face recognized: ', dj_decision[0], ' as member ', username)
+#         user_owns_lock = dj_decision[0] == lock.user.pk
+#         confidence_acceptable = dj_decision[1] < 45
+#         matching_rfid = event.RFID == lock.RFID
+#         print('User has access to lock: ', user_owns_lock)
+#         print('Confidence acceptable: ', confidence_acceptable, dj_decision[1])
+#         print('RFID matches: ', matching_rfid)
+#
+#         if user_owns_lock and confidence_acceptable and matching_rfid:
+#             serial = lock.serial
+#             data = {
+#                 'event_id': event.pk,
+#                 'action': 'unlock',
+#                 'serial': serial,
+#                 'mtype': 'fr'
+#             }
+#             lock.status = 'pending'
+#             lock.save()
+#             print('User authorized, sending unlock request.')
+#             requests.post('http://52.43.75.183:5000', json=data)
+#             return
+#         print('Access Denied.')
