1a4ca86a0a0fa89da3eba3364f9c5ebe59842df1
Steven Than <steventhan11@gmail.com>
2016-10-14 04:47:45 -0700

    more stylings

--- securedpi/views.py
+++ securedpi/views.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 
 class DashboardView(TemplateView):
     """Establish class for Dashboard page view."""
-    template_name = 'securedpi/dashboard_base.html'
+    template_name = 'securedpi/dashboard.html'
 
     def get_context_data(self, **kwargs):
         context = super(DashboardView, self).get_context_data(**kwargs)
